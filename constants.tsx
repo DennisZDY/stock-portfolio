4cd3710b49a8b157b1d711477a85b25ed9f1cf1c
DennisZDY <108217651+DennisZDY@users.noreply.github.com>
2025-12-05 18:32:17 +0800

    Update constants.tsx

--- constants.tsx
+++ constants.tsx
@@ -1,12 +1,10 @@
-import { Holding, RiskLevel, StrategyType, SoldTransaction } from './types';
+import { Holding, SoldTransaction } from './types';
 
 // --- 預設設定 (您可以在這裡修改預設值) ---
 export const DEFAULT_CONFIG = {
   initialCapital: 5000000, // 預設本金
   discount: 1.7            // 預設手續費折數
 };
-
-const generateId = () => Math.random().toString(36).substr(2, 9);
 
 // 預設持股：改為空陣列，讓您開啟時是乾淨的
 export const MOCK_HOLDINGS: Holding[] = [];
